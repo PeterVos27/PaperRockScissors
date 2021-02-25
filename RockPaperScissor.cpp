aef6e52218e9ccaecdc29a3b0bd73689e2ab4ff6
PeterVos27 <79651615+PeterVos27@users.noreply.github.com>
2021-02-25 16:20:01 +0100

    Create RockPaperScissor.cpp

---
+++ RockPaperScissor.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int input;
+string ai_response;
+int replay;
+int win;
+
+string answer(int user_input);
+int outcome(int user_input, string ai_input);
+
+int main()
+{
+    cout << "\n\n\n\nWelcome to my little game. This is a small game i made for the first time!\n\n";
+    cout << "You are going to play Rock, Paper, Scissors against me.\n\n";
+    cout << "Please enter the number of your choice of attack and press 'Enter'\n";
+    
+    do
+    {
+        cout << "Press...\n";
+        cout << "[1] for Rock,\n";
+        cout << "[2] for Paper'\n";
+        cout << "[3] for Scissors\n";
+        cin >> input;
+
+        ai_response = answer(input);
+
+        cout << "Your opponent chose... " << ai_response << "\n\n\n";
+
+
+        int win = outcome(input,ai_response);
+        if(win == 0)
+        {
+            cout << "Press 1 for 'Yes', 0 for 'No'";
+            cin >> replay;
+        }
+        else {
+            replay = 0;
+            cout << "Okay Bye.... :-(\n";
+        }
+
+    } while (replay == 1);
+    
+}
+
+
+// Opponent answer function
+string answer(int user_input){
+
+    if (user_input == 1){
+
+        cout << "You chose Rock. Your opponent is deciding...\n";
+
+        return "Paper";
+    }
+    else if (user_input == 2){
+
+        cout << "You chose Paper. Your opponent is deciding...\n";
+
+        return "Scissors";
+    }
+    else {
+        cout << "You chose Scissors. Your opponent is deciding...\n";
+
+        return "Rock";
+    }
+}
+
+
+// check outcome function
+int outcome(int user_input, string ai_input){
+    
+    if((user_input == 1 && ai_input == "Paper") ||
+        (user_input == 2 && ai_input == "Scissors") ||
+        (user_input == 3 && ai_input == "Rock"))
+        {
+            cout << "Sorry...you lost......Play again? :-D\n";
+            return 0;
+        }
+    else 
+    {
+        cout << "Yes...You won...I dont want to play anymore...Byebye";
+        return 1;
+    }    
+}
